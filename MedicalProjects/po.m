c5b5348dd6c61de1514d7f95dd26873899cbea33
Marco Velluto <velluto93@gmail.com>
2013-06-11 15:15:14 +0200

    yest

---
+++ MedicalProjects/po.m
@@ -0,0 +1,34 @@
+//
+//  po.m
+//  MedicalProjects
+//
+//  Created by Marco Velluto on 11/06/13.
+//  Copyright (c) 2013 Algos. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface po : XCTestCase
+
+@end
+
+@implementation po
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
